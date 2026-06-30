0c6c8988546a039dbd2b4ce038e101eb797ee39d
Aditi Rajput <aditirajput805@gmail.com>
2026-06-30 10:27:29 +0530

    Remove unused code: dead lib/component files, stale MySQL migrations, unused npm deps

--- lib/db/schema.ts
+++
@@ -1,8 +0,0 @@
-// Schema definitions have moved to lib/db/models/
-// This file kept for backwards-compatibility imports only.
-
-export const yjsToBase64 = (data: Uint8Array): string =>
-  Buffer.from(data).toString('base64');
-
-export const base64ToYjs = (data: string): Uint8Array =>
-  new Uint8Array(Buffer.from(data, 'base64'));
